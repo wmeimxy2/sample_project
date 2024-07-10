90dbda1dbcfaf1f5fe2ac5daaf79dc43665b73f0
wmeimxy2 <wmei@corevitas.com>
2024-07-10 14:50:17 -0400

    test main branch

--- sample_code.R
+++ sample_code.R
@@ -4,3 +4,5 @@ library(tidyverse)
 # Read in the most recent datacut from the PsO registry
 setwd("C:/Users/WinnieMei/Corrona LLC")
 test_data <- read_dta("Biostat Data Files - PsO (1)/datacut/data/test/20240510/ANA_TAE_07may2024.dta")
+
+# test
